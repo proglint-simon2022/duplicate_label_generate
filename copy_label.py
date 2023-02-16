155948a6a51148340e51912b427967e191d1d321
proglint-simon2022 <121565459+proglint-simon2022@users.noreply.github.com>
2023-02-17 01:02:35 +0530

    Update copy_label.py

--- copy_label.py
+++ copy_label.py
@@ -12,6 +12,7 @@ for session_id in os.listdir(duplicate_path):
     for sub_folder in os.listdir(f'{duplicate_path}\{session_id}'):
         dest_path = f'{duplicate_path}\{session_id}\{sub_folder}'
         for image in os.listdir(dest_path):
+            img_name = image[:-4]
             name_list = image.split('_')
             name = ''.join(name_list[:-1])
             for label in all_label_list:
@@ -19,7 +20,7 @@ for session_id in os.listdir(duplicate_path):
                 label_new = ''.join(label_list[:-1])
                 if label_new == name:
                     content = open(f'{label_path}\{label}', 'r').read()
-                    with open(dest_path + "/" + image+".txt", "a") as f:
+                    with open(dest_path + "/" + img_name[:-4]+".txt", "a") as f:
                         for txt in content:
                             f.write(f'{txt}')
                     f.close()
